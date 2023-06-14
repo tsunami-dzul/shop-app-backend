c393c8a6cb63d4039e3952fac0267b5caabd188c
Esdras <xdzul18@gmail.com>
2023-06-14 13:27:49 -0600

    task-6: Add logic for SQS

--- import-service/handler.js
+++ import-service/handler.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const AWS = require('aws-sdk');
+const { SQSClient, SendMessageCommand } = require('aws-sdk/client-sqs');
 const BUCKET = 'products-catalog-csv-file';
 
 module.exports.importProductsFile = async (event) => {
@@ -20,8 +21,10 @@ module.exports.importProductsFile = async (event) => {
 };
 
 module.exports.importFileParser = async (event) => {
+	const sqsClient = new SQSClient();
 	const s3 = new AWS.S3({ region: 'us-west-1' });
 	const params = {};
+	const body = [];
 
 	for (const record of event.Records) {
 		params = {
@@ -33,13 +36,32 @@ module.exports.importFileParser = async (event) => {
 	const s3Stream = s3.getObject(params).createReadStream();
 
 	s3Stream
-		.on('data', (data) => {
-			console.log(data);
+		.on('data', (chunk) => {
+			body.push(chunk);
 		})
 		.on('error', (error) => {
 			console.log(error);
 		})
 		.on('end', () => {
-			console.log('All data was processed');
+			const parsedBody = Buffer.concat(body).toString();
+			const records = parsedBody.split('\n');
+
+			for (let record of records) {
+				const product = record.split(',');
+				const p = {
+					id: product[0].id,
+					title: product[1].title,
+					description: product[2].description,
+					price: product[3].price,
+				};
+
+				sqsClient.send(
+					new SendMessageCommand({
+						QueueUrl:
+							'https://sqs.us-west-1.amazonaws.com/334657772509/product-service-dev-catalogItemsQueue',
+						MessageBody: p,
+					})
+				);
+			}
 		});
 };
